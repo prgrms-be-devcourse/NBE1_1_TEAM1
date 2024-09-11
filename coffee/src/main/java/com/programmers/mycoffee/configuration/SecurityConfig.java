94ea19b1df1b1770d52b2b6aa47294e56555e5c9
dowoo kim <dowoo12345@naver.com>
2024-09-11 17:34:21 +0900

    fix: jwt authentication

--- coffee/src/main/java/com/programmers/mycoffee/configuration/SecurityConfig.java
+++ coffee/src/main/java/com/programmers/mycoffee/configuration/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.programmers.mycoffee.configuration;
 
+import com.programmers.mycoffee.jwt.JwtFilter;
 import com.programmers.mycoffee.jwt.JwtUtil;
 import com.programmers.mycoffee.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -57,6 +63,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/admin/join", "/api/v1/admin/login").permitAll()
                         .anyRequest().authenticated());
         http
+                .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+        http
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
@@ -64,7 +72,29 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        //CORS 설정
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("access"));
+
+                        return configuration;
+                    }
+                })));
         return http.build();
     }
+
+
 }
 
--- coffee/src/main/java/com/programmers/mycoffee/service/admin/CustomUserDetails.java
+++ coffee/src/main/java/com/programmers/mycoffee/service/admin/CustomUserDetails.java
@@ -1,12 +1,13 @@
 package com.programmers.mycoffee.service.admin;
 
 import com.programmers.mycoffee.model.admin.AdminEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
-
+@Slf4j
 public class CustomUserDetails implements UserDetails {
     private final AdminEntity admin;
 
@@ -29,6 +30,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getPassword() {
+        log.info("password {} ", admin.getPassword());
         return admin.getPassword();
     }
 
