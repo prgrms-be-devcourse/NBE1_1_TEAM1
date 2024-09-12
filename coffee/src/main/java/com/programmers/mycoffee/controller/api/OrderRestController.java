0a8d2b4b3d2f410acbf444ede2d2510913793df3
[한도윤] <semin98@naver.com>
2024-09-12 10:03:04 +0900

    주문 관리기능

--- coffee/src/main/java/com/programmers/mycoffee/controller/api/OrderRestController.java
+++ coffee/src/main/java/com/programmers/mycoffee/controller/api/OrderRestController.java
@@ -3,14 +3,10 @@ package com.programmers.mycoffee.controller.api;
 import com.programmers.mycoffee.controller.CreateOrderRequest;
 import com.programmers.mycoffee.model.Email;
 import com.programmers.mycoffee.model.Order;
-import com.programmers.mycoffee.model.OrderStatus;
 import com.programmers.mycoffee.service.OrderService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderRestController {
@@ -29,46 +25,5 @@ public class OrderRestController {
                 orderRequest.postcode(),
                 orderRequest.orderItems()
         );
-    }
-
-    @GetMapping("/api/v1/{orderId}")
-    public ResponseEntity<Order> getOrderById(@PathVariable UUID orderId) {
-        Order order = orderService.findById(orderId);
-        if(order != null){
-            return new ResponseEntity<>(order, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/api/v1/all")
-    public ResponseEntity<List<Order>> showAll(){
-        List<Order> orders = orderService.findAll();
-        return new ResponseEntity<>(orders, HttpStatus.OK);
-    }
-
-    @PutMapping("/api/v1/update/{orderId}")
-    public ResponseEntity<Order> updateOrder(@PathVariable UUID orderId, @RequestBody CreateOrderRequest orderRequest) {
-        Order existOrder = orderService.findById(orderId);
-        if(existOrder == null){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-
-        existOrder.setAddress(orderRequest.address());
-        existOrder.setPostcode(orderRequest.postcode());
-        existOrder.setOrderStatus(OrderStatus.ACCEPTED);
-
-        Order updatedOrder = orderService.update(existOrder);
-        return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
-    }
-
-    @GetMapping("/api/v1/product")
-    public ResponseEntity<Order> findByProductName(@RequestParam String productName) {
-        Order order = orderService.findByName(productName);
-        if (order != null) {
-            return new ResponseEntity<>(order, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
     }
 }
--- coffee/src/main/java/com/programmers/mycoffee/service/OrderService.java
+++ coffee/src/main/java/com/programmers/mycoffee/service/OrderService.java
@@ -5,16 +5,7 @@ import com.programmers.mycoffee.model.Order;
 import com.programmers.mycoffee.model.OrderItem;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface OrderService {
     Order createOrder(Email email, String address, String postcode, List<OrderItem> orderItems);
-
-    List<Order> findAll();
-
-    Order update(Order order);
-
-    Order findById(UUID orderID);
-
-    Order findByName(String productName);
 }
